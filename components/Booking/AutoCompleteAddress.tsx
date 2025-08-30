27956a60563592c49fc59830d76f0acfe60fab4c
brej-29 <brejesh.bala@gmail.com>
2025-08-30 22:35:18 +0530

    Car type selection, Address auto completed, map box configured and payment method added

--- components/Booking/AutoCompleteAddress.tsx
+++ components/Booking/AutoCompleteAddress.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function AutoCompleteAddress() {
+    const [source,setSource]=useState<any>()
+    const [addressList,setAddressList]=useState<any>([]);
+    
+    useEffect(()=>{
+        const delayDebounceFn=  setTimeout(()=>{
+            getAddressList()
+        },1000)
+        return () => clearTimeout(delayDebounceFn) 
+    },[source])
+
+    const getAddressList=async()=>{
+        const res=await fetch('/api/search-address?q='+source,{
+            headers:{
+                "Content-Type": "application/json",  
+        }
+        });
+
+        const result=await res.json();
+        setAddressList(result)
+    }
   return (
-    <div>AutoCompleteAddress</div>
+    <div className='mt-5'>
+        <div>
+            <label className='font-semibold'>Pickup Address</label>
+            <input type="text"
+            className='bg-white p-1 border-[1px]
+            rounded-md w-full outline-none focus:bg-yellow-300'
+            value={source}
+            onChange={(e)=>setSource(e.target.value)}
+            />
+            {addressList?.suggestions?
+            <div className='shadow-md p-1 rounded-md
+            absolute w-full bg-white z-20'>
+            {addressList?.suggestions.map((item:any,index:number)=>(
+                <h2 key={index} className='p-3 hover:bg-gray-100
+                cursor-pointer'
+                >{item.full_address}</h2>
+            ))}
+           </div>:null}
+
+            <label className='font-semibold'>Drop Address</label>
+            <input type="text"
+            className='bg-white p-1 border-[1px]
+            rounded-md w-full outline-none focus:bg-yellow-300'
+            />
+        </div>
+    </div>
   )
 }
 
--- components/Booking/Booking.tsx
+++ components/Booking/Booking.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 import AutoCompleteAddress from './AutoCompleteAddress'
+import Cars from './Cars';
+import Cards from './Cards';
 
 function Booking() {
+    const [screenHeight, setScreenHeight] = useState<number | null>(null);
+
+    useEffect(() => {
+        setScreenHeight(window.innerHeight);
+    }, []);
+
   return (
     <div className='p-5 '>
         <h2 className='text-[20px] font-semibold'>Booking</h2>
         <div className='border-[1px] p-5 
         rounded-md' >
         <AutoCompleteAddress/>
+        <Cars/>
+        <Cards/>
     <button className='w-full
          bg-yellow-400
         p-1 rounded-md
